121c983bae7634012f45675cc80f43d7f8f4384b
LuckyStar <hadyenvip@gmail.com>
2015-06-23 22:36:35 +0800

    Init ExampleProject

---
+++ Example/Example/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Example
+//
+//  Created by Ethan Yang on 15/6/23.
+//  Copyright © 2015年 Ethan Yang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
