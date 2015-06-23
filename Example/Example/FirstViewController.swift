b9d8fe9be0c7cdb399e3bf10f2c069a500686ea4
LuckyStar <hadyenvip@gmail.com>
2015-06-23 23:10:10 +0800

    bundle resources

--- Example/Example/FirstViewController.swift
+++ Example/Example/FirstViewController.swift
@@ -8,13 +8,15 @@
 
 import UIKit
 import FontAwesomeKitSwift
+
 class FirstViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-//        FontAwesomeKitSwift.Icon.ico
+        let image = Icon.icons(FontAwesome.Adjust,FontAwesome.Adn)(size: 250).imageWithSize(CGSize(width: 250, height: 250))
+        imageView.image = image;
     }
 
     override func didReceiveMemoryWarning() {
